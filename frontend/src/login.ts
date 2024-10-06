b33ac63bc678f797264919d0d961585a13b9b817
Thales de Paula Martins <contaprofissional7@outlook.com>
2024-10-06 15:50:09 -0300

    Esquleto do redirecionamento da página de login

--- frontend/src/login.ts
+++ frontend/src/login.ts
@@ -1,23 +1,26 @@
-async function login(email: string, password: string) {
+async function login(email: string, senha: string) {
     try {
         const response = await fetch('http://localhost:8080/api/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ email, password }),
+            body: JSON.stringify({ email, senha }),
         });
 
         if (response.ok) {
             const data = await response.json();
-            console.log('Login successful:', data);
-            // Redirecionar ou salvar o token de autenticação, se houver
+            console.log('Login bem-sucedido:', data);
+            // Redirecionar para a página que exibe os dados do voluntário
+            window.location.href = '../templates/posauth.html'; // Verifique o caminho correto
         } else {
             const errorData = await response.json();
-            console.error('Login failed:', errorData);
+            console.error('Falha no login:', errorData);
+            // Exibir alerta com a mensagem de erro
+            window.alert('Login falhou: ' + errorData);
         }
     } catch (error) {
-        console.error('Error:', error);
+        console.error('Erro:', error);
     }
 }
 
@@ -25,8 +28,13 @@ const loginForm = document.getElementById('loginForm') as HTMLFormElement;
 
 loginForm.addEventListener('submit', (event) => {
     event.preventDefault();
+    console.log('Formulário enviado');
+
     const email = (document.getElementById('email') as HTMLInputElement).value;
     const senha = (document.getElementById('senha') as HTMLInputElement).value;
+
+    console.log('Email:', email);
+    console.log('Senha:', senha);
 
     login(email, senha);
 });
